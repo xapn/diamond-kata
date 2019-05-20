7786d752aefacc31e715ce3d5f66f1730fe93354
Xavier Pigeon <craftsman@chrysocode.io>
2019-05-20 14:18:35 +0200

    It should create a symmetrical, alphabetical sequence as a diamond given B.
    - Transform nil into constant.

--- src/test/java/kata/DiamondTest.java
+++ src/test/java/kata/DiamondTest.java
@@ -45,7 +45,7 @@ class DiamondTest {
                     .of(CODE_OF_A, givenLetter.getCode())
                     .map(code -> code == CODE_OF_A ? valueOf((char) code.intValue()) : valueOf(
                             (char) code.intValue()) + valueOf((char) code.intValue()))
-                    .collect(joining());
+                    .collect(joining("", "", "A"));
         }
     }
 
@@ -97,7 +97,7 @@ class DiamondTest {
 
         @Test
         void should_create_a_diamond_given_B() {
-            resultOf(() -> diamondOf("B")).isEqualTo("ABB");
+            resultOf(() -> diamondOf("B")).isEqualTo("ABBA");
         }
     }
 
